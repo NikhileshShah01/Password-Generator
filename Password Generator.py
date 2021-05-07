b752b5347e86ab48251f272fd46aa3f2146ac73a
Nikhilesh Shah <nikhileshshah10@gmail.com>
2021-05-07 18:21:44 +0530

    Add files via upload

---
+++ Password Generator.py
@@ -0,0 +1,9 @@
+import random
+smaller="abcdefghijklmnopqrstuvwxyz"
+captial="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers="1234567890"
+symbols="[]{}-_*;/\,."
+combine=smaller+captial+numbers+symbols
+length=15
+password="".join(random.sample(combine,length))
+print(password)
