cef776023ddb2d2d5c29b640dcb1b38010a42370
Nikhilesh Shah <nikhileshshah10@gmail.com>
2021-05-16 17:36:28 +0530

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -2,7 +2,7 @@ import random
 smaller="abcdefghijklmnopqrstuvwxyz"
 captial="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 numbers="1234567890"
-symbols="[]{}-_*;/\,."
+symbols="-+*&%$!@_."
 combine=smaller+captial+numbers+symbols
 length=15
 password="".join(random.sample(combine,length))
